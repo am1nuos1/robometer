e94e14c1446ebbfbcf06923fb257e440d07a7f7d
am1nuos1 <chaoxiang0930@gmail.com>
2026-05-12 23:22:20 -0700

    update eval_commands/batch_inference.sh

--- eval_commands/batch_inference.sh
+++ eval_commands/batch_inference.sh
@@ -1,26 +1,37 @@
 cd /home/czhang30/robometer
 
-# Defaults from scripts/batch_inference_local.py:
-#   --model-path: required, no default
-#   --input-dir: default None
-#   --video: default []
-#   --pattern: default "*.mp4"
-#   --task: default None
-#   --task-file: default None
-#   --out-dir: default None, which means sibling "output/" next to input dir
-#   --fps: default 1.0
-#   --max-frames: default 8
-#   --resize-max-side: default 224; use 0 to disable resizing
-#   --debug-memory: default false
-#   --success-threshold: default 0.5
+.venv/bin/python scripts/batch_inference_local.py \
+  --model-path robometer/Robometer-4B \
+  --input-dir /home/czhang30/robometer/my_dataset/test1/input \
+  --pattern "*.mp4" \
+  --task-file /home/czhang30/robometer/my_dataset/test1/task_description.txt \
+  --out-dir /home/czhang30/robometer/my_dataset/test1/output \
+  --fps 1.0 \
+  --max-frames 8 \
+  --resize-max-side 224 \
+  --success-threshold 0.5
+
+
 
 .venv/bin/python scripts/batch_inference_local.py \
   --model-path robometer/Robometer-4B \
   --input-dir /home/czhang30/robometer/my_dataset/Krisha_dataset/head/observation.images.head \
   --pattern "*.mp4" \
   --task-file /home/czhang30/robometer/my_dataset/test1/task_description.txt \
-  --out-dir /home/czhang30/robometer/my_dataset/test1/output \
   --fps 3.0 \
   --max-frames 30 \
   --resize-max-side 224 \
   --success-threshold 0.5
+
+
+# letf arm
+# /home/czhang30/robometer/my_dataset/Left_arm_picks_a_cube_places_in_box/task_description.txt
+.venv/bin/python scripts/batch_inference_local.py \
+  --model-path robometer/Robometer-4B \
+  --input-dir /home/czhang30/robometer/my_dataset/Left_arm_picks_a_cube_places_in_box/head/observation.images.head \
+  --task-file /home/czhang30/robometer/my_dataset/Left_arm_picks_a_cube_places_in_box/task_description.txt \
+  --fps 3.0 \
+  --max-frames 30 \
+  --resize-max-side 224 \
+  --success-threshold 0.5
+/home/czhang30/robometer/my_dataset/Left_arm_picks_a_cube_places_in_box/left_wrist/observation.images.left_wrist
